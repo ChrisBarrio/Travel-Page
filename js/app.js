3335d9c904188d482ffa772ef3bce5f0ce811676
ChrisBarrio <ch.barrionuevo89@gmail.com>
2022-06-16 12:25:13 -0300

    commit 'centado de divs en responsive'

--- js/app.js
+++ js/app.js
@@ -59,16 +59,18 @@ var swiper = new Swiper(".mySwiperReview", {
       prevEl: ".swiper-button-prev",
     },
     // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    420: {
-      slidesPerView: 1,
-      spaceBetween: 30
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+      },
+      // when window width is >= 480px
+      420: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
     },
     // when window width is >= 640px
     640: {
